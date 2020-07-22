abb9286292a3810258630d27b985258256657d5c
DongkyooLee <wind.dong.dream@>
2020-07-22 18:57:14 +0900

    레이블 이미지 DB 구현

--- src/main/java/com/gomson/gomsonServer/label/LabelImageRepository.java
+++ src/main/java/com/gomson/gomsonServer/label/LabelImageRepository.java
@@ -1,4 +1,31 @@
 package com.gomson.gomsonServer.label;
 
-public class LabelRepository {
+import com.gomson.gomsonServer.domain.LabelImage;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class LabelImageRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    public boolean save(List<LabelImage> labelImageList) {
+        try {
+            for (LabelImage labelImage : labelImageList) {
+                manager.persist(labelImage);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public List<LabelImage> findList(Integer scoredBy) {
+        return manager.createNativeQuery("SELECT l FROM LabelImage l WHERE l.scoredBy = :scoredBy ORDER BY l.id ASC OFFSET 0 LIMIT 10", LabelImage.class)
+                .setParameter("scoredBy", scoredBy).getResultList();
+    }
 }
