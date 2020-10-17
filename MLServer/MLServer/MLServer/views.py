fecd6a347cf45e2b00bbaca27f58fa43a0871689
이동규 <wind.dong.dream@gmail.com>
2020-10-17 15:15:14 +0900

    [ML서버] 세그멘테이션 API 제공

--- MLServer/MLServer/MLServer/views.py
+++ MLServer/MLServer/MLServer/views.py
@@ -10,6 +10,7 @@ from MLServer.MLServer.tryangle_models import User
 from process.guider import Guider, LineComponent
 import process.color as color_guide
 from process.object import Human, Object
+import matplotlib.pyplot as plt
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -31,7 +32,16 @@ class ImageSegmentationView(APIView):
         # image = cv2.resize(image, None, fx=0.2, fy=0.2, interpolation=cv2.INTER_AREA)
 
         r = api.segment(image)
-        return Response(r, status=status.HTTP_200_OK, content_type='application/json')
+        body = {'result': list()}
+        for index in range(r['rois'].shape[0]):
+            body['result'].append({
+                'rois': list(r['rois'][index]),
+                'class_ids': r['class_ids'][index],
+                'mask': [list(y) for y in r['masks'][:, :, index]]
+            })
+            plt.imshow(r['masks'][:, :, index])
+            plt.show()
+        return Response(str(body), status=status.HTTP_200_OK, content_type='application/json')
 
 
 class ImageGuideView(APIView):
@@ -39,7 +49,7 @@ class ImageGuideView(APIView):
         file = request.FILES['file']
         image = cv2.imdecode(np.frombuffer(file.file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
         dominant_colors = list(color_guide.find_dominant_colors(image))
-        guider = Guider(image)
+        guider = Guider(image, False)
 
         component_list = list()
         for component in guider.component_list:
@@ -51,12 +61,21 @@ class ImageGuideView(APIView):
 
                 if isinstance(component.object, Human):
                     data['pose'] = component.object.pose_class
+                    pose_points = list()
+                    for i in range(component.object.pose.shape[0]):
+                        if component.object.pose[i][2] > 0.05:
+                            pose_points.append(list(component.object.pose[i, :2]))
+                        else:
+                            pose_points.append(None)
+                    data['pose_points'] = pose_points
 
                 data['id'] = component.id
                 data['class'] = component.object.clazz
                 data['center_point_x'] = component.object.center_point[0]
                 data['center_point_y'] = component.object.center_point[1]
                 data['area'] = component.object.area
+                data['mask'] = [list(y) for y in component.object.mask]
+                data['roi'] = list(component.object.roi)
                 component_dict['ObjectComponent'] = data
                 component_list.append(component_dict)
 
@@ -66,6 +85,5 @@ class ImageGuideView(APIView):
             "dominant_color_list": dominant_colors,
             "image_size": list(image.shape[:2])
         }
-        print(body)
         return Response(str(body), status=status.HTTP_200_OK, content_type='application/json')
 
